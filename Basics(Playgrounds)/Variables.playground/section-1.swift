43ce9c21647df4a4c420f9e187a6a9fb97adb8e2
Michael Angelo <yamikamisama@gmail.com>
2015-02-22 19:45:35 -0500

    add Variables playground example

---
+++ Basics(Playgrounds)/Variables.playground/section-1.swift
@@ -0,0 +1,20 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var firstInteger:Int = 0, secondInteger:Int = 29
+var thirdInteger:Double = -1000858
+var x:Int = 5
+
+
+//I will be using this later to track the discounts at this Department store seasonally
+var discountAtDepartmentStore:Double = 0.3
+
+/*
+
+Paragraph1: Explanation
+
+Paragraph2: Explanation
+
+*/
+
