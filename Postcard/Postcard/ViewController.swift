cafb9bfef544ea7a2b6c0ade4ee787d094a9ae87
Michael Angelo <yamikamisama@gmail.com>
2015-02-22 19:02:29 -0500

    remove keyboard on loss of focus

--- Postcard/Postcard/ViewController.swift
+++ Postcard/Postcard/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     
     @IBOutlet weak var enterNameTextField: UITextField!
-    
     @IBOutlet weak var enterMessageTextField: UITextField!
 
     @IBOutlet weak var mailButton: UIButton!
@@ -32,17 +31,20 @@ class ViewController: UIViewController {
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
-        messageLabel.textColor = UIColor.blueColor()
+        messageLabel.textColor = UIColor.redColor()
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
         
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
-        
-        nameLabel.hidden = false
-        nameLabel.text = enterNameTextField.text
-        nameLabel.textColor = UIColor.blueColor()
-    }
+        }
 
 }
 
